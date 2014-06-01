12010417b22547352d098595e678c3fce1588a50
Artur Z <arjamizo@gmail.com>
2014-06-01 06:18:09 +0200

    converted to YUV format therefore adding shapes works for videotestsrc. Problem 'buffer not long enough' 'solved' by dividing width and height by 2

--- intercept-vstream.py
+++ intercept-vstream.py
@@ -6,6 +6,9 @@ import pygst
 pygst.require("0.10")
 import gst
 import md5
+import traceback
+import cairo
+from math import pi
 
 class NewElement(gst.Element):
 	""" A basic, buffer forwarding gstreamer element """
@@ -60,7 +63,45 @@ class NewElement(gst.Element):
 		#and then push a buffer to the next element, returning a value saying
 		# it was either successful or not.
 		outbuf = buf.copy_on_write()
+		self.draw_on(outbuf)
 		return self.srcpad.push(outbuf)
+		
+	def draw_on (self, buf):
+		try:
+			caps = buf.get_caps()
+			width = caps[0]['width']
+			height = caps[0]['height']
+			framerate = caps[0]['framerate']
+			width, height = width/2, height/2
+			surface = cairo.ImageSurface.create_for_data (buf, cairo.FORMAT_ARGB32, width, height, 4 * width)
+			ctx = cairo.Context(surface)
+		except:
+			print "Failed to create cairo surface for buffer"
+			traceback.print_exc()
+			return
+
+		try:
+			center_x = width/4
+			center_y = 3*height/4
+
+			# draw a circle
+			radius = float (min (width, height)) * 0.25
+			ctx.set_source_rgba (0.0, 0.0, 0.0, 0.9)
+			ctx.move_to (center_x, center_y)
+			ctx.arc (center_x, center_y, radius, 0, 2.0*pi)
+			ctx.close_path()
+			ctx.fill()
+			ctx.set_source_rgba (1.0, 1.0, 1.0, 1.0)
+			ctx.set_font_size(0.3 * radius)
+			txt = "Hello World"
+			extents = ctx.text_extents (txt)
+			ctx.move_to(center_x - extents[2]/2, center_y + extents[3]/2)
+			ctx.text_path(txt)
+			ctx.fill()
+
+		except:
+			print "Failed cairo render"
+			traceback.print_exc()
 
 #here we register our class with glib, the c-based object system used by
 #gstreamer
@@ -70,7 +111,7 @@ class GTK_Main:
 
 	def __init__(self):
 		window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-		window.set_title("Webcam-Viewer")
+		window.set_title("Intercepting web-cam")
 		window.set_default_size(500, 400)
 		window.connect("destroy", gtk.main_quit, "WM destroy")
 		vbox = gtk.VBox()
@@ -92,11 +133,16 @@ class GTK_Main:
 
 		# Set up the gstreamer pipeline
 		self.player = gst.Pipeline("player")
-		src = gst.element_factory_make("v4l2src", "camera")
+		# src = gst.element_factory_make ("v4l2src")
+		src = gst.element_factory_make ("videotestsrc")
+		cf = gst.element_factory_make ("capsfilter")
+		WIDTH, HEIGHT, FRAMERATE = 640, 480, 15
+		caps = gst.caps_from_string ("video/x-raw-yuv,format=(fourcc)YUY2,width=%d,height=%d,framerate=%d/1" % (WIDTH, HEIGHT, FRAMERATE))
+		cf.set_property ("caps", caps)
 		filter = NewElement()
 		sink = gst.element_factory_make('autovideosink')
-		self.player.add(src, filter, sink)
-		gst.element_link_many(src, filter, sink)
+		self.player.add(src, cf, filter, sink)
+		gst.element_link_many(src, cf, filter, sink)
 
 		bus = self.player.get_bus()
 		bus.add_signal_watch()
