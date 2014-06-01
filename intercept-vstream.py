31917a1b0c321fab21f1321a23079614a36cdec7
Artur Z <arjamizo@gmail.com>
2014-06-01 05:54:03 +0200

    added custom filtering element

--- intercept-vstream.py
+++ intercept-vstream.py
@@ -5,6 +5,65 @@ import pygtk, gtk, gobject
 import pygst
 pygst.require("0.10")
 import gst
+import md5
+
+class NewElement(gst.Element):
+	""" A basic, buffer forwarding gstreamer element """
+
+	#here we register our plugin details
+	__gstdetails__ = (
+		"NewElement plugin",
+		"newelement.py",
+		"gst.Element, that passes a buffer from source to sink (a filter)",
+		"Stephen Griffiths <scgmk5@gmail.com>")
+	
+	#source pad (template): we send buffers forward through here
+	_srctemplate = gst.PadTemplate ('src',
+		gst.PAD_SRC,
+		gst.PAD_ALWAYS,
+		gst.caps_new_any())
+
+	#sink pad (template): we recieve buffers from our sink pad
+	_sinktemplate = gst.PadTemplate ('sink',
+		gst.PAD_SINK,
+		gst.PAD_ALWAYS,
+		gst.caps_new_any())
+	
+	#register our pad templates
+	__gsttemplates__ = (_srctemplate, _sinktemplate)
+
+	def __init__(self, *args, **kwargs):   
+		#initialise parent class
+		gst.Element.__init__(self, *args, **kwargs)
+		
+		#source pad, outgoing data
+		self.srcpad = gst.Pad(self._srctemplate)
+		
+		#sink pad, incoming data
+		self.sinkpad = gst.Pad(self._sinktemplate)
+		self.sinkpad.set_setcaps_function(self._sink_setcaps)
+		self.sinkpad.set_chain_function(self._sink_chain)
+		
+		#make pads available
+		self.add_pad(self.srcpad)
+		self.add_pad(self.sinkpad)
+
+	def _sink_setcaps(self, pad, caps):
+		#we negotiate our capabilities here, this function is called
+		#as autovideosink accepts anything, we just say yes we can handle the
+		#incoming data
+		return True
+
+	def _sink_chain(self, pad, buf):
+		print "filter", len(buf), md5.new(buf).hexdigest()
+		#this is where we do filtering
+		#and then push a buffer to the next element, returning a value saying
+		# it was either successful or not.
+		return self.srcpad.push(buf)
+
+#here we register our class with glib, the c-based object system used by
+#gstreamer
+gobject.type_register(NewElement)
 
 class GTK_Main:
 
@@ -33,9 +92,10 @@ class GTK_Main:
 		# Set up the gstreamer pipeline
 		self.player = gst.Pipeline("player")
 		src = gst.element_factory_make("v4l2src", "camera")
+		filter = NewElement()
 		sink = gst.element_factory_make('autovideosink')
-		self.player.add(src, sink)
-		gst.element_link_many(src, sink)
+		self.player.add(src, filter, sink)
+		gst.element_link_many(src, filter, sink)
 
 		bus = self.player.get_bus()
 		bus.add_signal_watch()
