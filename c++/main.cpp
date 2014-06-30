1c51b2b996c72171e5b7a96c7ec64d535d56b791
Artur Z <arjamizo@gmail.com>
2014-06-30 02:18:29 +0200

    some how-to-run instructions in comments

--- c++/main.cpp
+++ c++/main.cpp
@@ -5,7 +5,13 @@ Works with
 CLIENTIP=192.168.103 gst-launch-1.0 -v v4l2src ! timeoverlay shaded-background=true text="pi" ! video/x-raw,height=480,width=640,framerate=30/1 ! videoconvert ! omxh264enc ! rtph264pay ! udpsink host=$CLIENTIP port=5000
 OR
 http://wiki.oz9aec.net/index.php/Raspberry_Pi_Camera#Gstreamer_using_RTP.2FUDP
-
+CLIENTIP=192.168.103 raspivid -n -w 1280 -h 720 -b 4500000 -fps 30 -vf -hf -t 0 -o - | \
+	gst-launch-1.0 -v fdsrc !  h264parse ! rtph264pay config-interval=10 pt=96 ! \
+	udpsink host=$CLIENTIP port=9000
+sudo apt-get install libgstreamer1.0-dev
+sudo apt-get install gstreamer1.0-libav
+gst-launch-1.0 -v udpsrc port=9000 caps='application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264' ! \
+     rtph264depay ! avdec_h264 ! videoconvert ! autovideosink sync=false
 */
 #include <stdlib.h>
 #include <stdio.h>
@@ -23,6 +29,7 @@ int main(int argc, char *argv[]) {
 	GError *error = NULL;
 	/* Build the pipeline */
 
+	//Compiling: g++ main.cpp `pkg-config --cflags --libs gstreamer-1.0 gtk+-2.0`; raspivid -n -t 1000000 -vf -b 2000000 -fps 25 -o - | GST_DEBUG=3 ./a.out
 	//! x264enc pass=qual quantizer=20 tune=zerolatency
 	pipeline = gst_parse_launch("fdsrc fd=0 ! h264parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! udpsink host=192.168.0.104 port=1234", &error);
 	//pipeline = gst_parse_launch("v4l2src device=/dev/video0 ! video/x-raw-yuv,width=640,height=480 ! x264enc pass=qual quantizer=20 tune=zerolatency ! rtph264pay ! udpsink host=127.0.0.1 port=1234", &error);
